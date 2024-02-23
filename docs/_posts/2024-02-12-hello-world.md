1c47c92cf8d4c726a57b3a426a9ea75d9c792350
Ronak Pradeep <ronakice@hotmail.com>
2024-02-23 17:13:51 +0100

    update website

---
+++ docs/_posts/2024-02-12-hello-world.md
@@ -0,0 +1,19 @@
+---
+title: "TREC 2024 RAG - Hello World!"
+date: 2024-02-12T08:00:00-00:00
+categories:
+  - Annoucements
+tags:
+  - TREC 2024
+classes: wide
+toc: true
+---
+
+Hello World.
+
+Best regards,
+TREC-RAG Organizers
+
+## Useful links
+
+None for now :-).
--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "TREC 2024 RAG - Hello World!",
+        "excerpt":"Hello World.   Best regards, TREC-RAG Organizers   Useful links   None for now :-).  ","categories": ["Annoucements"],
+        "tags": ["TREC 2024"],
+        "url": "/annoucements/hello-world/",
+        "teaser": null
+      }]
